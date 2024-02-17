c0432ff8379b9de37633c071e1b5e12604e770ae
Prim <artstudioexp@gmail.com>
2024-02-17 15:09:17 +0300

    Modify prim_exception to use exception type

--- prim_exception.hpp
+++ prim_exception.hpp
@@ -3,21 +3,26 @@
 
 #include <string>
 
-#define PRIM_EXCEPTION(what) prim::Exception(what, __FILE__, __LINE__)
+#define PRIM_EXCEPTION(what, type) prim::Exception(what, type, __FILE__, __LINE__)
+
+enum class ExceptionType { OperationalException, NotFound };
+inline const char* ExceptionType_str[] = { "OperationalException", "NotFound" };
 
 namespace prim
 {
 
-class Exception
-{
-private:
-    std::string whatString;
-public:
-    Exception(): whatString("[PRIM EXCEPTION]") {}
-    Exception(std::string what, std::string file, int line): whatString("[PRIM EXCEPTION] -> File: '" + file + "', Line: '" + std::to_string(line) + "', What: '" + what + "'.") {}
+    class Exception
+    {
+    private:
+        std::string whatString;
+        ExceptionType type;
+    public:
+        Exception(): whatString("[PRIM EXCEPTION]") {}
+        Exception(std::string what, ExceptionType type, std::string file, int line): whatString(std::string("[PRIM EXCEPTION : ") + ExceptionType_str[(u_int32_t)type] + "] -> File: '" + file + "', Line: '" + std::to_string(line) + "', What: '" + what + "'.") {}
 
-    inline std::string what() const { return whatString; }
-};
+        std::string what() const { return whatString; }
+        ExceptionType type() const { return type; };
+    };
 
 }
 
