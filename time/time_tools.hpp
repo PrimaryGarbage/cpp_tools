fab120158ca1f4d3cf3d0294e5f940dd227ea0bd
Prim <artstudioexp@gmail.com>
2024-02-17 16:29:03 +0300

    Add time tools

---
+++ time/time_tools.hpp
@@ -0,0 +1,19 @@
+#ifndef __TIME_TOOLS_HPP__
+#define __TIME_TOOLS_HPP__
+
+#include <string>
+#include <chrono>
+#include <ctime>
+
+namespace prim
+{
+    std::string currentDateTimeString(const char* format)
+    {
+        std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+        std::string timeStr(30, '\0');
+        std::strftime(&timeStr[0], timeStr.size(), format, std::localtime(&time));
+        return timeStr;
+    }
+}
+
+#endif // __TIME_TOOLS_HPP__
